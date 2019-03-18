cf679e0a9ab79a724eab07dfacccab281dab9669
talentless_guy <pilll.PL22@gmail.com>
2019-03-18 16:52:27 +0300

    replacing mjs with esm, configuring nodemon

---
+++ server/main.js
@@ -0,0 +1,37 @@
+import Koa from 'koa'
+import next from 'next'
+import Router from 'koa-router'
+
+const dev = process.env.NODE_ENV !== 'production'
+const n = next({ dev }), handle = n.getRequestHandler()
+
+const { log } = console
+
+log(`App is running in ${!process.env.NODE_ENV ? 'developemt' : 'production'} mode.`)
+
+n.prepare().then(() => {
+  const app = new Koa(), router = new Router()
+
+  router.get('/profile/:profile', async ctx => {
+    await n.render(ctx.req, ctx.res, '/profile', ctx.params)
+    ctx.respond = false
+  })
+    
+  router.get('/group/:group', async ctx => {
+    await n.render(ctx.req, ctx.res, '/group', ctx.params)
+    ctx.respond = false
+  })
+
+  router.get('*', async ctx => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
+  })
+
+  app.use(async (ctx, next) => {
+    ctx.res.statusCode = 200
+    if (ctx.status !== 200) {
+        n.render(ctx.req, ctx.res, '/_error')
+    }
+    await next()
+  }).use(router.routes()).listen(80, () => log('Go to localhost, bro'))
+})
