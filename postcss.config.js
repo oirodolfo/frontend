022582ec0f3a2fcc47b0050a3ea0fe154b177dce
talentless_guy <pilll.PL22@gmail.com>
2019-05-04 18:14:04 +0300

    add codacy ignore files

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-	plugins: [
-		autoprefixer()
-	]
+    plugins: [autoprefixer()]
 }
