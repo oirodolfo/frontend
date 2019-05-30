ceae3a9ada3b42fc84097c179dc75e1a74b3bdb6
talentless_guy <pilll.PL22@gmail.com>
2019-05-30 21:47:59 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPlugins = require('next-compose-plugins')
 const css = require('@zeit/next-css')
 const ts = require('@zeit/next-typescript')
 
-module.exports = withPlugins(
-    [ts, css]
-)
+module.exports = {
+	...withPlugins([ts, css]),
+	target: 'serverless'
+}
