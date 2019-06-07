993828e7e97c44f01f72636d3fb6e3a922b563e4
talentless_guy <pilll.PL22@gmail.com>
2019-06-07 19:53:41 +0300

    update info

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,36 @@ import withData from '../lib/apollo'
 
 const str = `
 query data {
-	hello
+  hello
 }
 `
 
 const App = () => (
   <div>
     <h1>Komfy</h1>
-    <h2>Our first GraphQL query! 🎉</h2>
+    <div>
+      <a href="https://github.com/komfy" rel="noopener norefferer" target="_blank">
+        GitHub
+      </a>
+      &nbsp;
+      <a href="https://crowdforge.io/projects/534" rel="noopener norefferer" target="_blank">
+        CrowdForge
+      </a>
+    </div>
+    <h2>Project status ⚡</h2>
+    <p className="status">
+      Currently we are working on UI library and API so during next months there won&apos;t be any huge changes here.
+      Please stand by until we release v0.0.1 of both API and UI library so we can begin building the website itself.
+    </p>
+    I&apos; answer all questions here: <a href="t.me/talentless_guy">@talentless_guy</a>
     <Query query={gql(str)}>
       {({ loading, error, data }: QueryResult) => {
         if (loading) return <p>Loading</p>
         if (error) return <p>Error</p>
         return (
           <div>
-            <h3>Query:</h3>
-            <pre>{str}</pre>
             <p>
-              Output: <b>{data.hello}</b>
+              GraphQL request: <b>{JSON.stringify(data)}</b>
             </p>
           </div>
         )
