cbe7ec065561df1c9881def5bece219116129438
talentlessguy <pilll.PL22@gmail.com>
2019-08-03 15:10:04 +0300

    trying to fix env

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withImages = require('next-images')
 module.exports = withImages({
   target: 'serverless',
   env: {
-    API: process.env.API
+    API: process.env.NODE_ENV === 'production' ? 'https://komfy.now.sh' : 'http://localhost:8080'
   }
 })
